719e2ba70d384adffc755a14e38d0d27cbc581a0
so1gging <65271008+so1gging@users.noreply.github.com>
2021-06-05 14:40:12 +0900

    model 설정

---
+++ src/main/java/com/example/model/TodoResponse.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponse {
+    private Long id;
+    private String title;
+    private Long order;
+    private Boolean completed;
+    private String url;
+
+    public TodoResponse(TodoEntitiy todoEntitiy) {
+        this.id = todoEntitiy.getId();
+        this.title = todoEntitiy.getTitle();
+        this.order = todoEntitiy.getOrder();
+        this.completed = todoEntitiy.getCompleted();
+
+        this.url = "http://localhost:8080/" + this.id;
+    }
+}
